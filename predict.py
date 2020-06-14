ff22cc2fffb9d5ad85d707f17c47983bbb6ca0fc
dudefeng <1220335783@qq.com>
2020-06-14 10:33:21 +0800

    YOLOv3

---
+++ predict.py
@@ -0,0 +1,21 @@
+from nets.yolo3 import yolo_body
+from keras.layers import Input
+from yolo import YOLO
+from PIL import Image
+
+yolo = YOLO()
+
+while True:
+    img = input('Input image filename:')
+    if img == 'q':
+            break
+    try:
+
+        image = Image.open(img)
+    except:
+        print('Open Error! Try again!')
+        continue
+    else:
+        r_image = yolo.detect_image(image)
+        r_image.show()
+yolo.close_session()
